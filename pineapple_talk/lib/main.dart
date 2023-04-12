70afa5099d9f10e89b83c97badaa7dce3a21a2c7
Hayeong Shim <shyme2055@gmail.com>
2023-04-13 02:28:42 +0900

    Create Project

--- pineapple_talk/lib/main.dart
+++ pineapple_talk/lib/main.dart
@@ -13,14 +13,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Pineapple Talk',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      child: GestureDetector(
+        onTap: () {
+          textFocusId.unfocus();
+          textFocusPw.unfocus();
+        },
+        child: MaterialApp(
+          title: 'Pineapple Talk',
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          ),
+          home: MyHomePage(),
         ),
-        home: MyHomePage(),
+
       ),
+
     );
   }
 }
@@ -29,17 +37,69 @@ class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 }
 
+// TBD - refactoring
+FocusNode textFocusId = FocusNode();
+FocusNode textFocusPw = FocusNode();
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          children: [
+            SizedBox(height: 70.0),
+            Column(
+              children: [
+                Image.asset('assets/images/Logo.png', height: 130),
+                Text(
+                  'Fine Apple',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.green,
+                  )),
+              ],
+            ),
+            SizedBox(height: 30.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: false,
+                labelText: 'Username',
+              ),
+              focusNode: textFocusId,
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: false,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+              focusNode: textFocusPw,
+            ),
+            SizedBox(height: 30.0),
+            ButtonBar(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    textFocusId.unfocus();
+                    textFocusPw.unfocus();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0)
+                    ),
+                    minimumSize: Size(410, 50),
+                  ),
+                  child: Text("LOGIN"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
